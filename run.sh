99c801fb17021fc097c66d7e7bb2bbcb61ff2e39
themis0888 <themis0888@gmail.com>
2018-08-11 00:33:11 +0900

    Prnet custom init

---
+++ run.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 python demo.py -i /shared/data/sample/ \
+-o /shared/data/sample/prnet_out/ --isDlib True \
+--isKpt True --isPose True --isShow True --isImage True 
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+        name='nsml example 10 ladder_network',
+        version='1.0',
+        description='ns-ml',
+        install_requires=[
+            'matplotlib',
+            'tqdm',
+            'pillow',
+            'numpy',
+			'scikit-image',
+			'scipy',
+            'opencv-python',
+            'dlib'
+
+        ]
+)
